565323656949ae79c6d07311a7ab984db2338deb
Przemyslaw Rudzki <przemek.rudzki@gmail.com>
2020-01-29 14:32:32 -0500

    updated generattion function

--- script.js
+++ script.js
@@ -1,77 +1,157 @@
-var numberList = '0123456789';
-var letterList = 'abcdefghijklmnopqrstuvwxyz';
-var specialList = '!@#$%^&*()_+=?<>,.';
-var passLenght = 8;
-var includeUpperCase = true;
-var includeLowerCase = true;
-var includeNumber = true;
-var includeSpecial = true;
-var password = '';
-/*
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-*/
-
+//take random character from input string.
 function generateRandCharacter(str)
   {
     var num = Math.floor(Math.random()*str.length);
     return str[num];
   }
 
-  for (var k=1;k<=passLenght;k++)
-    {
-      //if (inc)
-      password += generateRandCharacter(letterList);
-    }
-
-
+//disables generate button if user selects no options
 function disableButton()
     {
-      if (document.querySelectorAll("input")[0].checked == false &&
-      document.querySelectorAll("input")[1].checked == false &&
-      document.querySelectorAll("input")[2].checked == false &&
-      document.querySelectorAll("input")[3].checked == false)
-      { 
-        document.getElementById("generate").disabled = true;
-      }
-      else document.getElementById("generate").disabled = false;
+      var checkbox = document.querySelectorAll("input");
+      var button = document.getElementById("generate");
+      if ( checkbox[0].checked == false &&
+           checkbox[1].checked == false &&
+           checkbox[2].checked == false &&
+           checkbox[3].checked == false )
+          { 
+            button.disabled = true;
+          }
+        else button.disabled = false;
     }
 
 function generatePassword()
     {
-      //do somethin
+      var digits = '0123456789';
+      var lowers = 'abcdefghijklmnopqrstuvwxyz';
+      var uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      var symbols ='!@#$%^&*()_+=?<>,.';
+      var workingString = '';
+      var password = '';
+      var pattSpec = /[!@#$%^&*()_+=?<>,.]/;
+      var pattUp = /[A-Z]/;
+      var pattLo = /[a-z]/;
+      var pattDig = /\d/;
 
-      console.log('Generating...' + slider.value);
+      var checkbox = document.querySelectorAll("input");  
+
+      if (checkbox[0].checked)
+        {
+          workingString += uppers;
+        }
+      if (checkbox[1].checked)
+        {
+          workingString += lowers;
+        }
+      if (checkbox[2].checked)
+        {
+          workingString += digits;
+        }
+      if (checkbox[3].checked)
+        {
+          workingString += symbols;
+        }
       
+      while (true) {
+
+        password = '';
+        for (n=1;n<=slider.value;n++)
+        {
+            password += generateRandCharacter(workingString);
+        }
+    
+        if (checkbox[0].checked)
+            {
+                if (!pattUp.test(password))
+                    {
+                        continue;
+                    }
+            }
+           else {
+              if (pattUp.test(password))
+                {
+                  continue;
+                }
+            } 
+    
+        if (checkbox[1].checked)
+            {
+                if (!pattLo.test(password))
+                    {
+                        continue;
+                    }
+            }
+            else {
+              if (pattLo.test(password))
+                {
+                  continue;
+                }
+            } 
+        
+        if (checkbox[2].checked)
+            {
+                if (!pattDig.test(password))
+                    {
+                        continue;
+                    }
+            }
+          else
+              {
+                if (pattDig.test(password))
+                  {
+                    continue;
+                  }
+              }
+    
+        if (checkbox[3].checked)
+            {
+                if (!pattSpec.test(password))
+                    {
+                        continue;
+                    }
+            }
+         else  {
+            if ( pattSpec.test(password) )
+              {
+                continue;
+              }
+          } 
+        document.getElementById("password").value = password;
+        break;
+
+    
+    }
+  }
+//copy password to clipboard
+function copyPass()
+    {
+      var copyText = document.getElementById('password');
+      copyText.select();
+      copyText.setSelectionRange(0,9999);
+      document.execCommand('copy');
+      alert("Copied the text: " + copyText.value);
     }
 
+//init function
+//adding listeneres, setting values     
 function init()
     {
+      var slider = document.getElementById("slider");
+      var output = document.getElementById("passlen");
+      output.value = slider.value; 
+      slider.oninput = function()
+       {
+          output.value = this.value;
+       }
       document.querySelectorAll("input")[0].addEventListener("click",disableButton);
       document.querySelectorAll("input")[1].addEventListener("click",disableButton);
       document.querySelectorAll("input")[2].addEventListener("click",disableButton);
       document.querySelectorAll("input")[3].addEventListener("click",disableButton);
       document.getElementById("generate").addEventListener("click",generatePassword);
+      document.getElementById("copy").addEventListener("click",copyPass);
       
-      var slider = document.getElementById("slider");
-      var output = document.getElementById("passlen");
-      output.value = slider.value; 
-      slider.oninput = function() {
-          output.value = this.value;
-      }
-    }
+    }   
 
 init();
